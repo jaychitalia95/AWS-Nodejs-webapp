aeb57abb3390e64b801a8a119896c1521303f69a
Jay Chitalia <jay.chitalia@validity.com>
2020-01-16 10:55:57 -0500

    Made the route table main instead of the default one

--- infrastructure/aws/terraform/networking/main.tf
+++ infrastructure/aws/terraform/networking/main.tf
@@ -18,12 +18,16 @@ data "aws_availability_zones" "available" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy="default"
+  tags = {
+    Name = "testVPC-jchitalia"
+  }
 }
 
 resource "aws_subnet" "primary" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch= true
 
   tags = {
     Name = "primary-jchitalia"
@@ -33,6 +37,8 @@ resource "aws_subnet" "primary" {
 resource "aws_subnet" "secondary" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch= true
+
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
@@ -50,15 +56,20 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_route_table" "rtb" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block        = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
   tags = {
     Name = "rtb-jchitalia"
   }
+}
+
+resource "aws_route" "route-main" {
+  route_table_id = aws_route_table.rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.rtb.id
 }
 
 resource "aws_route_table_association" "subnetprimary" {
